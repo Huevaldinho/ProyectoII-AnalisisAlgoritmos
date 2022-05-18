459a4637313547b7865bcafd57e25370773e93cb
Huevaldinho <47650141+Huevaldinho@users.noreply.github.com>
2022-05-17 20:30:24 -0600

    Cambios
    Corregi una parte del error pero aun hay casos donde no se obtiene la solucion mas optima.

--- ProyectoII-AnalisisAlgoritmos/src/proyectoii/analisisalgoritmos/ProgramacionDinamica.java
+++ ProyectoII-AnalisisAlgoritmos/src/proyectoii/analisisalgoritmos/ProgramacionDinamica.java
@@ -31,13 +31,14 @@ public class ProgramacionDinamica{
         for (int i=0;i<presentaciones.length;i++){
             System.out.println("\t Presentacion de "+presentaciones[i]+" :"+combinacionPresentaciones[i]);
             totalKilos+=  combinacionPresentaciones[i]*presentaciones[i];
-            if (combinacionPresentaciones[i]!=0){//Si es 0 es porque no se usa esa presentacion.
-                
-                desperdicio+= (kilos- (combinacionPresentaciones[i]*presentaciones[i]));
-            }
+//            if (combinacionPresentaciones[i]!=0){//Si es 0 es porque no se usa esa presentacion.
+//                
+//                desperdicio+= (kilos- (combinacionPresentaciones[i]*presentaciones[i]));
+//            }
         }
+        //Desperdicio es: kilos - suma de todos los kilos por presentacion.
         System.out.println("Total kilos: "+totalKilos);
-        System.out.println("Desperdicio: "+desperdicio);
+        System.out.println("Desperdicio: "+Math.abs(kilos-totalKilos)+'\n');
     }
     /**
      *  Metodo para calcular la combinacion que desperdicia menos arroz. Programacion Dinamica
@@ -62,7 +63,7 @@ public class ProgramacionDinamica{
         //con un numero grande porque no se puede obtener n kilos con presentacion
         //de 0 kilos.
         for (int columna = 1; columna <= kilos; columna++)
-            matriz[0][columna] = 1000;
+            matriz[0][columna] = 100;//Integer.MAX_VALUE;
         
         //Variable para escoger cual combinacion que desperdicia menos kilos.
         int minimoDesperdicio;
@@ -80,11 +81,15 @@ public class ProgramacionDinamica{
                     /*
                         Si el valor de la presentacion es menor o igual al kilo
                         Saca la combinacion que desperdicie menos la combinacion
-                        de la celda de arriba o la combinacion de
-                        kilo - redondearArriba(kilo/presentacion)*presentacion.
-                        columna - (int) Math.ceil(columna/presentacion[fila-1]) * presentacion[fila-1]
+                    de la celda de arriba o sea, con presentaciones mas pequenas o
+                    con la combinacion que se genera de restar kilo - presentacion.
+                    Porque n se puede construir con muchas presentaciones.
+                    Ejemplo. si se tiene 5 puedo hacer 5-3 que es 2, entonces yo se
+                    que puedo comprar una presentacion de 2 sin tener desperdicio y 
+                    si le agrego una de 3, obtengo 5 kilos.
                     */
-                    minimoDesperdicio = min(matriz[fila - 1][columna] , (columna - (int )Math.ceil(columna/presentaciones[fila-1])*presentaciones[fila-1]));
+                    //Math.abs((columna - (int )Math.ceil(columna/presentaciones[fila-1])*presentaciones[fila-1]))
+                    minimoDesperdicio = min(matriz[fila - 1][columna] , Math.abs(matriz[fila][columna-1-fila]));//-1 porque empieza en 1
                     matriz[fila][columna] = minimoDesperdicio;
                 }
             }
@@ -151,11 +156,57 @@ public class ProgramacionDinamica{
                 //Se se salta a la columna con valor kilo - presesentacion.
             }
         }
+        //imprimirMatriz(matrizResultado,presentaciones);
         return combinacion;
     }
-
+    static void imprimirMatriz(int [][]matriz,int array[]){
+//        int fila=0;
+//        int columna=0;
+//        while (fila < matriz.length){
+//            while (columna<matriz[fila].length){
+//                System.out.println("Fila: "+fila+" - Columna: "+columna+" = "+matriz[fila][columna]);
+//                columna++;
+//            }
+//            System.out.println("");
+//            fila++;
+//            columna=0;
+//        }
+        for (int x=0; x < matriz.length; x++) {
+          System.out.print("|");
+          for (int y=0; y < matriz[x].length; y++) {
+            System.out.print (matriz[x][y]);
+            if (y!=matriz[x].length-1) System.out.print("\t");
+          }
+          System.out.println("|");
+        }
+    }
     public static void main(String[] args) {
-        ProgramacionDinamica c = new ProgramacionDinamica(10, new int[]{2,3,7});
+        ProgramacionDinamica a = new ProgramacionDinamica(10, new int[]{2,3,7});
+        a.getCombinacion();
+
+        ProgramacionDinamica b = new ProgramacionDinamica(11, new int[]{2,3,7});
+        b.getCombinacion();
+
+        ProgramacionDinamica c = new ProgramacionDinamica(5, new int[]{2,3,7});
         c.getCombinacion();
+        
+        ProgramacionDinamica d = new ProgramacionDinamica(102, new int[]{2,3,7,10});
+        d.getCombinacion();
+        
+        ProgramacionDinamica e = new ProgramacionDinamica(55, new int[]{2,3,7,5,10});
+        e.getCombinacion();
+        
+        ProgramacionDinamica f= new ProgramacionDinamica(71, new int[]{3,5,11});
+        f.getCombinacion();
+        
+        ProgramacionDinamica g= new ProgramacionDinamica(71, new int[]{11,13,100});
+        g.getCombinacion();
+        //Este deberia ser 5*20 =100 + 13*1 = 113
+        ProgramacionDinamica h= new ProgramacionDinamica(113, new int[]{5,11,13});
+        h.getCombinacion();
+        
+        ProgramacionDinamica i= new ProgramacionDinamica(8, new int[]{2,3,5});
+        i.getCombinacion();
+        //IMPORTANTE ORDENAR LAS PRESENTACIONES ANTES DE TODO.
     }
 }
